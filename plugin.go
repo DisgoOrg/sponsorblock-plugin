f71718b0f2eab92a83e1136f17072e0575167864
Toπ <git@topi.wtf>
2023-08-21 02:49:03 +0200

    added missing event handlers

--- plugin.go
+++ plugin.go
@@ -1,16 +1,10 @@
 package sponsorblock
 
 import (
-	"bytes"
-	"context"
 	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
 
 	"github.com/disgoorg/disgolink/v3/disgolink"
 	"github.com/disgoorg/disgolink/v3/lavalink"
-	"github.com/disgoorg/snowflake/v2"
 )
 
 var (
@@ -23,16 +17,14 @@ func New() *Plugin {
 		eventPlugins: []disgolink.EventPlugin{
 			&segmentsLoadedHandler{},
 			&segmentSkippedHandler{},
-		},
-		httpClient: &http.Client{
-			Timeout: 10 * time.Second,
+			&chaptersLoadedHandler{},
+			&chapterStartedHandler{},
 		},
 	}
 }
 
 type Plugin struct {
 	eventPlugins []disgolink.EventPlugin
-	httpClient   *http.Client
 }
 
 func (p *Plugin) EventPlugins() []disgolink.EventPlugin {
@@ -45,62 +37,6 @@ func (p *Plugin) Name() string {
 
 func (p *Plugin) Version() string {
 	return "1.0.0"
-}
-
-func (p *Plugin) GetCategories(ctx context.Context, node disgolink.Node, guildID snowflake.ID) (categories []string, err error) {
-	path := fmt.Sprintf("%s/v3/sessions/%s/players/%d/sponsorblock/categories", node.Config().RestURL(), node.SessionID(), guildID)
-
-	err = p.do(node, ctx, http.MethodGet, path, nil, &categories)
-	return
-}
-
-func (p *Plugin) SetCategories(ctx context.Context, node disgolink.Node, guildID snowflake.ID, categories []string) error {
-	path := fmt.Sprintf("%s/v3/sessions/%s/players/%d/sponsorblock/categories", node.Config().RestURL(), node.SessionID(), guildID)
-
-	return p.do(node, ctx, http.MethodPut, path, categories, nil)
-}
-
-func (p *Plugin) DeleteCategories(ctx context.Context, node disgolink.Node, guildID snowflake.ID) error {
-	path := fmt.Sprintf("%s/v3/sessions/%s/players/%d/sponsorblock/categories", node.Config().RestURL(), node.SessionID(), guildID)
-
-	return p.do(node, ctx, http.MethodDelete, path, nil, nil)
-}
-
-func (p *Plugin) do(node disgolink.Node, ctx context.Context, method string, path string, rqBody any, rsBody any) error {
-
-	var rqBuff *bytes.Buffer
-	if rqBody != nil {
-		rqBuff = &bytes.Buffer{}
-		if err := json.NewEncoder(rqBuff).Encode(rqBody); err != nil {
-			return err
-		}
-	}
-
-	rq, err := http.NewRequestWithContext(ctx, method, path, rqBuff)
-	if err != nil {
-		return err
-	}
-	rq.Header.Set("Authorization", node.Config().Password)
-	if rqBody != nil {
-		rq.Header.Set("Content-Type", "application/json")
-	}
-
-	rs, err := p.httpClient.Do(rq)
-	if err != nil {
-		return err
-	}
-	defer rs.Body.Close()
-
-	if rs.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to set categories: %s", rs.Status)
-	}
-
-	if rsBody != nil {
-		if err = json.NewDecoder(rs.Body).Decode(rsBody); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 var _ disgolink.EventPlugin = (*segmentsLoadedHandler)(nil)
@@ -132,6 +68,40 @@ func (h *segmentSkippedHandler) OnEventInvocation(player disgolink.Player, data 
 	var e SegmentSkippedEvent
 	if err := json.Unmarshal(data, &e); err != nil {
 		player.Lavalink().Logger().Error("Failed to unmarshal SegmentSkipped Event", err)
+		return
+	}
+
+	player.Lavalink().EmitEvent(player, e)
+}
+
+type chaptersLoadedHandler struct{}
+
+func (h *chaptersLoadedHandler) Event() lavalink.EventType {
+	return EventTypeChaptersLoaded
+}
+
+func (h *chaptersLoadedHandler) OnEventInvocation(player disgolink.Player, data []byte) {
+	var e ChaptersLoadedEvent
+	if err := json.Unmarshal(data, &e); err != nil {
+		player.Lavalink().Logger().Error("Failed to unmarshal ChaptersLoaded Event", err)
+		return
+	}
+
+	player.Lavalink().EmitEvent(player, e)
+}
+
+var _ disgolink.EventPlugin = (*chapterStartedHandler)(nil)
+
+type chapterStartedHandler struct{}
+
+func (h *chapterStartedHandler) Event() lavalink.EventType {
+	return EventTypeChapterStarted
+}
+
+func (h *chapterStartedHandler) OnEventInvocation(player disgolink.Player, data []byte) {
+	var e ChapterStartedEvent
+	if err := json.Unmarshal(data, &e); err != nil {
+		player.Lavalink().Logger().Error("Failed to unmarshal ChapterStarted Event", err)
 		return
 	}
 
